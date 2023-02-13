5ab80f3cfe4d22ef065945826b0bfcaa020a5367
Utk14007 <94047015+Utk14007@users.noreply.github.com>
2023-02-13 09:46:00 +0530

    Create utk.java

---
+++ utk.java
@@ -0,0 +1,9 @@
+    public class utk
+    {
+    public static void main(String args[])
+    {
+    int a=4;
+    int volume=a*a*a;
+    System.out.println("Volume ot the cube="+volume);
+    }
+    }
