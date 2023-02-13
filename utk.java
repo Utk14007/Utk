557b5fa17f67c31f71f966f44bd4d2c495077dca
Utk14007 <94047015+Utk14007@users.noreply.github.com>
2023-02-13 10:09:58 +0530

    Update utk.java

--- utk.java
+++ utk.java
@@ -1,9 +1,21 @@
-    public class utk
-    {
-    public static void main(String args[])
-    {
-    int a=4;
-    int volume=a*a*a;
-    System.out.println("Volume ot the cube="+volume);
-    }
-    }
+import java.util.Scanner;
+ 
+class utk
+{
+   public static void main(String args[])
+   {
+      String str;
+      String rev = "";
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter a string:");
+      str = sc.nextLine();
+      int length = str.length();
+      for ( int i = length - 1; i >= 0; i-- )
+         rev = rev + str.charAt(i);
+      if (str.equals(rev))
+         System.out.println(str+" is a palindrome");
+      else
+         System.out.println(str+" is not a palindrome");
+ 
+   }
+}
